e38fc4d3e16e27fe1deb2af17ed9d2b42f317004
QC3284 <2078725082@qq.com>
2025-07-09 01:00:29 +0800

    6

---
+++ script/lede-github-actions-rl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#本仓库及文件只在Github发布
+#作者：QC3284@github.com(https://github.com/QC3284)
+#本仓库地址：https://github.com/QC3284/openwrt-actions
+#最后更新时间：2025.7.9
+echo "自动编译 (Automatic compilation)" >> release.txt
+echo "使用源码 (Use source code)：" >> release.txt
+echo "[lede](https://github.com/coolsnowwolf/lede)" >> release.txt
+echo "$(date)" >> release.txt
