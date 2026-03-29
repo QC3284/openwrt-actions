91609f6d65a9d9c7f96eb6d7db027cb7a5917074
QC3284 <2078725082@qq.com>
2026-03-29 09:05:33 +0800

    Test-Update

--- script/immortalwrt-actions-txt.sh
+++ script/immortalwrt-actions-txt.sh
@@ -2,10 +2,10 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions
-#最后更新时间：2026.3.7
+#最后更新时间：2026.3.29
 echo "自动编译 (Automatic compilation)" >> release.txt
 echo "使用源码 (Use source code)：" >> release.txt
-echo "[padavanoly-immortalwrt](https://github.com/padavanonly/immortalwrt-mt798x-24.10)" >> release.txt
+echo "[immortalwrt-mt7981-rebase](https://github.com/chasey-dev/immortalwrt-mt798x-rebase)" >> release.txt
 echo "默认账号 (Default account)：" >> release.txt
 echo "root" >> release.txt
 echo "默认密码 (Default password)：" >> release.txt
